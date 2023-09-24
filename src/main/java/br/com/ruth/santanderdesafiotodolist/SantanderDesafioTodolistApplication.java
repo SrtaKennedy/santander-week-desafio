b1712fa7e55691c7a962379af34acbf4a48a303e
SrtaKennedy <ruthellen1800@gmail.com>
2023-09-24 19:01:18 -0300

    first commit

---
+++ src/main/java/br/com/ruth/santanderdesafiotodolist/SantanderDesafioTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.ruth.santanderdesafiotodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDesafioTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDesafioTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ruth/santanderdesafiotodolist/SantanderDesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ruth.santanderdesafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
